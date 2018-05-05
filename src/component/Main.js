e7ef35a706edde068c4404464b0bf57110f88e2b
stevewitman <stevewitman@gmail.com>
2018-05-05 14:13:23 -0600

    Create a sub-component called Main

--- src/component/Main.js
+++ src/component/Main.js
@@ -5,7 +5,7 @@ class Main extends Component {
     return (
       <div className="App">
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          [Main Component]
         </p>
       </div>
     );
