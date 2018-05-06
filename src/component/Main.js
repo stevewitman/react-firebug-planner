a108afe34957f4adb6a6dbbb224d1f61b3473678
stevewitman <stevewitman@gmail.com>
2018-05-05 22:10:12 -0600

    Working form that submits object with name, email, date, and sent

--- src/component/Main.js
+++ src/component/Main.js
@@ -3,23 +3,40 @@ import React, { Component } from 'react';
 import { connect } from 'react-firebase'
 
 class Main extends Component {
-  componentDidMount() {
-    this.props.setValue('hi from main')
+  // componentDidMount() {
+  //   this.props.setValue('hi from main')
+  // }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addItem(e.target.name.value, e.target.email.value);
+    e.target.name.value = ''; 
+    e.target.email.value = ''; 
   }
   render() {
     const { value } = this.props;
     return (
-      <div className="App">
-          [Main Component]
-          { value }
+      <div>
+        <p className="App">
+            { value }
+        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Enter Name" name="name" />
+          <input type="text" placeholder="Enter Email" name="email" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
 }
 
 const MapPropsToFirebase = (props, ref) => ({
-  value: 'helloWorld',
-  setValue: value => ref('helloWorld').set(value)
+  setValue: value => ref('helloWorld').set(value),
+  addItem: (name, email) => ref('item').push({
+    timeStamp: Date.now(),
+    name: name,
+    email: email,
+    sent: false
+  })
 })
 
-export default connect(MapPropsToFirebase)(Main );
+export default connect(MapPropsToFirebase)(Main);
